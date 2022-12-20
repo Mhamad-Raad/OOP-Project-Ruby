80ce5ec6ec1fadf8e0dad1f9d90a18c8fd8b1403
joker69216 <hamaraad883@gmail.com>
2022-12-20 12:03:50 +0300

    update person class

--- person_class.rb
+++ person_class.rb
@@ -2,6 +2,7 @@ class Person
   attr_accessor :name, :age, :id
 
   def initialize(age, name: 'Unkown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
